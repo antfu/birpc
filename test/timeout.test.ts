adb5fd850f588e3204034ea0406868390deff52b
Ari Perkkiö <ari.perkkio@gmail.com>
2024-01-26 13:11:10 +0200

    feat: add `onTimeoutError` option (#13)

--- test/timeout.test.ts
+++ test/timeout.test.ts
@@ -1,15 +1,16 @@
 import { MessageChannel } from 'node:worker_threads'
-import { expect, it } from 'vitest'
+import { expect, it, vi } from 'vitest'
 import { createBirpc } from '../src'
 import * as Bob from './bob'
 import type * as Alice from './alice'
 
 type AliceFunctions = typeof Alice
+type BobFunctions = typeof Bob
 
 it('timeout', async () => {
   const channel = new MessageChannel()
 
-  const bob = createBirpc<AliceFunctions>(
+  const bob = createBirpc<AliceFunctions, BobFunctions>(
     Bob,
     {
       post: data => channel.port1.postMessage(data),
@@ -24,5 +25,58 @@ it('timeout', async () => {
   }
   catch (e) {
     expect(e).toMatchInlineSnapshot('[Error: [birpc] timeout on calling "hello"]')
+  }
+})
+
+it('custom onTimeoutError', async () => {
+  const channel = new MessageChannel()
+  const onTimeout = vi.fn()
+
+  const bob = createBirpc<AliceFunctions, BobFunctions>(
+    Bob,
+    {
+      post: data => channel.port1.postMessage(data),
+      on: data => channel.port1.on('message', data),
+      timeout: 100,
+      onTimeoutError(functionName, args) {
+        onTimeout({ functionName, args })
+        throw new Error('Custom error')
+      },
+    },
+  )
+
+  try {
+    await bob.hello('Bob')
+    expect(1).toBe(2)
+  }
+  catch (e) {
+    expect(onTimeout).toHaveBeenCalledWith({ functionName: 'hello', args: ['Bob'] })
+    expect(e).toMatchInlineSnapshot(`[Error: Custom error]`)
+  }
+})
+
+it('custom onTimeoutError without custom error', async () => {
+  const channel = new MessageChannel()
+  const onTimeout = vi.fn()
+
+  const bob = createBirpc<AliceFunctions, BobFunctions>(
+    Bob,
+    {
+      post: data => channel.port1.postMessage(data),
+      on: data => channel.port1.on('message', data),
+      timeout: 100,
+      onTimeoutError(functionName, args) {
+        onTimeout({ functionName, args })
+      },
+    },
+  )
+
+  try {
+    await bob.hello('Bob')
+    expect(1).toBe(2)
+  }
+  catch (e) {
+    expect(onTimeout).toHaveBeenCalledWith({ functionName: 'hello', args: ['Bob'] })
+    expect(e).toMatchInlineSnapshot(`[Error: [birpc] timeout on calling "hello"]`)
   }
 })
