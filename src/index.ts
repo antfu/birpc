700806a8dc40fda5db88c7e8eef164a788b1962b
kongmoumou <kongmoumou666@gmail.com>
2024-09-06 23:47:48 +0800

    feat: support async iter as rpc result

--- src/index.ts
+++ src/index.ts
@@ -67,6 +67,10 @@ export type BirpcFn<T> = PromisifyFn<T> & {
    * Send event without asking for response
    */
   asEvent: (...args: ArgumentsType<T>) => void
+  /**
+   * Call fn as async iterator
+   */
+  asAsyncIter: (...args: ArgumentsType<T>) => ReturnType<T>
 }
 
 export interface BirpcGroupFn<T> {
@@ -131,6 +135,10 @@ interface Response {
    * Error
    */
   e?: any
+  /**
+   * Async iter done
+   */
+  d?: boolean
 }
 
 type RPCMessage = Request | Response
@@ -180,13 +188,13 @@ export function createBirpc<RemoteFunctions = Record<string, never>, LocalFuncti
         sendEvent.asEvent = sendEvent
         return sendEvent
       }
+      let id: string
       const sendCall = async (...args: any[]) => {
+        id = nanoid()
         // Wait if `on` is promise
         await _promise
-        return new Promise((resolve, reject) => {
-          const id = nanoid()
+        const callPromise = new Promise((resolve, reject) => {
           let timeoutId: ReturnType<typeof setTimeout> | undefined
-
           if (timeout >= 0) {
             timeoutId = setTimeout(() => {
               try {
@@ -205,9 +213,29 @@ export function createBirpc<RemoteFunctions = Record<string, never>, LocalFuncti
               timeoutId = timeoutId.unref?.()
           }
 
-          rpcPromiseMap.set(id, { resolve, reject, timeoutId })
+          rpcPromiseMap.set(id, { resolve, reject, timeoutId, ...rpcPromiseMap.get(id) })
           post(serialize(<Request>{ m: method, a: args, i: id, t: 'q' }))
         })
+        return callPromise
+      }
+      sendCall.asAsyncIter = function sendAsyncIterCall(...args: any[]) {
+        sendCall(...args)
+        async function * asyncIterWrapper() {
+          let res: undefined | { done: boolean, value: any }
+          do {
+            res = await new Promise((resolve, reject) => {
+              rpcPromiseMap.set(id, { ...rpcPromiseMap.get(id), resolve, reject })
+            })
+            if (typeof res?.done === 'undefined')
+              throw new Error('[birpc] function return is not async iterable')
+            if (res.done)
+              return
+            else
+              yield res.value
+          } while (true)
+        }
+        const iter = asyncIterWrapper()
+        return iter
       }
       sendCall.asEvent = sendEvent
       return sendCall
@@ -229,8 +257,19 @@ export function createBirpc<RemoteFunctions = Record<string, never>, LocalFuncti
       else {
         try {
           result = await fn.apply(rpc, args)
+
+          // handle async iter result
+          if (result[Symbol.asyncIterator]) {
+            for await (const iterRes of result) {
+              if (msg.i)
+                post(serialize(<Response>{ t: 's', i: msg.i, r: iterRes, d: false, e: error }), ...extra)
+            }
+            if (msg.i)
+              return post(serialize(<Response>{ t: 's', i: msg.i, r: undefined, d: true, e: error }), ...extra)
+          }
         }
         catch (e) {
+          result = undefined
           error = e
         }
       }
@@ -242,17 +281,27 @@ export function createBirpc<RemoteFunctions = Record<string, never>, LocalFuncti
       }
     }
     else {
-      const { i: ack, r: result, e: error } = msg
+      const { i: ack, r: result, e: error, d: done } = msg
       const promise = rpcPromiseMap.get(ack)
       if (promise) {
         clearTimeout(promise.timeoutId)
 
-        if (error)
-          promise.reject(error)
-        else
-          promise.resolve(result)
+        if (typeof done === 'boolean') {
+          if (error) { promise.reject(error) }
+          else {
+            promise.resolve({ done, value: result })
+            if (done)
+              rpcPromiseMap.delete(ack)
+          }
+        }
+        else {
+          if (error)
+            promise.reject(error)
+          else
+            promise.resolve(result)
+          rpcPromiseMap.delete(ack)
+        }
       }
-      rpcPromiseMap.delete(ack)
     }
   })
 
